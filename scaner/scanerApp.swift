f432b5b6301159fea2bb7c873bf39d1b1bdcab90
evan <liukersun@gmail.com>
2025-06-30 06:18:23 +0800

    Initial Commit

---
+++ scaner/scanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scanerApp.swift
+//  scaner
+//
+//  Created by Evan on 2025/6/30.
+//
+
+import SwiftUI
+
+@main
+struct scanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
